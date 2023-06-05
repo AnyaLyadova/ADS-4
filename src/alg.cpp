0227c62087dc537c0d21c1333f9dde824873c8cd
AnyaLyadova <124922006+AnyaLyadova@users.noreply.github.com>
2023-06-06 01:48:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,9 @@
 int countPairs1(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        k++;
+      for (int j = i + 1; j < len; j++) {
+          if (arr[i] + arr[j] == value) {
+              k++;
           }
       }
   }
@@ -14,27 +14,27 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int* arr, int len, int value) {
     int k = 0;
     for (int i = 0; i < len - 1; i++) {
-      for (int j = len - 1; i < j; j--) {
-        if (arr[i] + arr[j] == value) {
-          k++;
+                for (int j = len - 1; i < j; j--) {
+                    if (arr[i] + arr[j] == value) {
+                        k++;
                     }
                 }
             }
-  return k;
-  }
+            return k;
+        }
 
 int countPairs3(int* arr, int len, int value) {
     int k = 0;
     for (int i = 0; i < len - 1; i++) {
-      int leftBorder = i, rightBorder = len;
-      while (rightBorder - 1 > leftBorder) {
-        int mid = (leftBorder + rightBorder) / 2;
-        if (arr[i] + arr[mid] == value) {
-          k++;
-          int mid2 = mid + 1;
-          while (arr[i] + arr[mid2] == value && mid2 < rightBorder) {
-            k++;
-            mid2++;
+        int leftBorder = i, rightBorder = len;
+        while (rightBorder - 1 > leftBorder) {
+            int mid = (leftBorder + rightBorder) / 2;
+            if (arr[i] + arr[mid] == value) {
+                k++;
+                int mid2 = mid + 1;
+                while (arr[i] + arr[mid2] == value && mid2 < rightBorder) {
+                    k++;
+                    mid2++;
                 }
                 mid2 = mid - 1;
                 while (arr[i] + arr[mid2] == value && mid2 > leftBorder) {
